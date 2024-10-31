2be6cd1fa84ac567d332367f5a1728056c466aaf
EmirS-h <emirsahin2834@icloud.com>
2024-10-31 15:55:56 +0300

    Added sqldelight

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,17 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
 
     id("androidx.navigation.safeargs.kotlin") version "2.8.3" apply false
+
+    kotlin("jvm") version "2.0.21"
+    kotlin("plugin.serialization") version "2.0.21"
+
+    id("app.cash.sqldelight") version "2.0.2"
+}
+
+sqldelight {
+    databases {
+        create("AppDatabase") {
+            packageName.set("com.emirs.hackhaton")
+        }
+    }
 }
